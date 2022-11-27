0a0a5d4980abb510b54c92c45b5b0f8d332c3042
Yonatan Laurence <laurence0521@gmail.com>
2022-11-27 21:14:31 +0200

    Add Unit Test for Die

--- app/src/test/java/com/example/diceroller/DieTest.kt
+++ app/src/test/java/com/example/diceroller/DieTest.kt
@@ -1,17 +1,18 @@
 package com.example.diceroller
 
+import junit.framework.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class DieTest {
+    private val die = Die()
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun roll_returns_a_number_between_1_and_6() {
+        assertTrue("Roll returns a number between 1 and 6", die.roll() in 1..6)
     }
 }
